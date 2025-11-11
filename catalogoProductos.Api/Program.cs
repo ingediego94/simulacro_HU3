957bd54e83a407712e974bb4a07bafa12b7b554a
dv_csharp <ingediego94@gmail.com>
2025-11-11 13:28:29 -0500

    casi

--- catalogoProductos.Api/Program.cs
+++ catalogoProductos.Api/Program.cs
@@ -79,13 +79,23 @@ builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
-
+// Deploy
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI(c =>
     {
         c.SwaggerEndpoint("/swagger/v1/swagger.json", "Product Catalog & User management API v1");
+        c.RoutePrefix = string.Empty;
+    });
+}
+
+if (app.Environment.IsDevelopment() || app.Environment.EnvironmentName == "Local")
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Product Catalog API v1");
         c.RoutePrefix = string.Empty;
     });
 }
