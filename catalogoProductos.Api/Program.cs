bb3a135fe2dd2794d32563a74e0b61dfe3e9ff25
dv_csharp <ingediego94@gmail.com>
2025-11-11 13:25:45 -0500

    CASI

--- catalogoProductos.Api/Program.cs
+++ catalogoProductos.Api/Program.cs
@@ -79,6 +79,17 @@ builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Product Catalog & User management API v1");
+        c.RoutePrefix = string.Empty;
+    });
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
